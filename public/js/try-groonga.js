2865a2b44aba30bc455efea0a35cb2850c3343a3
Kouhei Sutou <kou@clear-code.com>
2014-07-20 11:10:17 +0900

    Untabify

--- public/js/try-groonga.js
+++ public/js/try-groonga.js
@@ -3,45 +3,45 @@ jQuery(function($, undefined) {
     var sendCommandQueue = [];
 
     function enqueueCommand(command, onResponse) {
-	var sendCommand = function() {
-	    var success = function(data, textStatus, jqXHR) {
-		var response;
-		if (typeof data == "string") {
-		    response = data;
-		} else {
-		    response = JSON.stringify(data, undefined, 2);
-		}
-		onResponse(response);
+        var sendCommand = function() {
+            var success = function(data, textStatus, jqXHR) {
+                var response;
+                if (typeof data == "string") {
+                    response = data;
+                } else {
+                    response = JSON.stringify(data, undefined, 2);
+                }
+                onResponse(response);
 
-		sendCommandQueue.shift();
-		if (sendCommandQueue.length > 0) {
-		    sendCommandQueue[0]();
-		}
-	    };
-	    $.post("sessions/" + id, command, success);
-	};
+                sendCommandQueue.shift();
+                if (sendCommandQueue.length > 0) {
+                    sendCommandQueue[0]();
+                }
+            };
+            $.post("sessions/" + id, command, success);
+        };
 
-	sendCommandQueue.push(sendCommand);
-	if (sendCommandQueue.length == 1) {
-	    sendCommand();
-	}
+        sendCommandQueue.push(sendCommand);
+        if (sendCommandQueue.length == 1) {
+            sendCommand();
+        }
     }
 
     $("#terminal").terminal(function(command, term) {
-	if (command === "") {
-	    return;
-	}
+        if (command === "") {
+            return;
+        }
 
-	enqueueCommand(command, function(response) {
-	    if (response.length == 0) {
-		return;
-	    }
-	    term.echo(response);
-	});
+        enqueueCommand(command, function(response) {
+            if (response.length == 0) {
+                return;
+            }
+            term.echo(response);
+        });
     }, {
-	greetings: "",
-	height: 400,
-	prompt: "groonga> "
+        greetings: "",
+        height: 400,
+        prompt: "groonga> "
     });
 
     $('#terminal').mousewheel(function(event) {
